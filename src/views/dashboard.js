f66b5e22abdf605891a11781dd5ec8ad8d5937d8
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2022-06-30 11:57:47 -0300

    Change dir structure

--- src/views/dashboard.js
+++ src/views/dashboard.js
@@ -1,6 +1,5 @@
-import { BarChart } from "../components/dashboard/barChart";
+import { ProfitabilityByGenre } from "./profitabilityByGenre";
 import { useData } from "../utils/getData";
-import { rollup, sum, group } from "d3";
 
 const csvUrl =
   "https://gist.githubusercontent.com/dmenezesgabriel/d138a218e5d6d064b40b45f693c27b42/raw/3919b288f6663e8c406d7286a78f23f0721fc752/HollywoodsMostProfitableStories.csv";
@@ -12,47 +11,5 @@ export const Dashboard = () => {
     return <pre>'loading...'</pre>;
   }
 
-  let profitabilityByGenre = Array.from(
-    rollup(
-      dataSet,
-      (v) => {
-        return sum(v, (d) => d.Profitability);
-      },
-      (d) => d.Genre
-    ),
-    ([genre, profitability]) => ({
-      genre,
-      profitability,
-    })
-  );
-
-  console.log();
-
-  const data = {
-    labels: profitabilityByGenre.map((d) => d.genre),
-    datasets: [
-      {
-        label: "Profitability",
-        data: profitabilityByGenre.map((d) => d.profitability),
-      },
-    ],
-  };
-
-  const options = {
-    plugins: {
-      datalabels: {
-        anchor: "end",
-        align: "top",
-        formatter: Math.round,
-        font: { weigth: "bold", size: 10 },
-      },
-    },
-    scales: { yAxis: { display: false } },
-  };
-
-  return (
-    <>
-      <BarChart data={data} options={options} />
-    </>
-  );
+  return <ProfitabilityByGenre dataSet={dataSet} />;
 };
---
+++ src/views/profitabilityByGenre.js
@@ -0,0 +1,42 @@
+import { BarChart } from "../components/dashboard/barChart";
+import { rollup, sum } from "d3";
+
+export const ProfitabilityByGenre = ({ dataSet }) => {
+  let profitabilityByGenre = Array.from(
+    rollup(
+      dataSet,
+      (v) => {
+        return sum(v, (d) => d.Profitability);
+      },
+      (d) => d.Genre
+    ),
+    ([genre, profitability]) => ({
+      genre,
+      profitability,
+    })
+  );
+
+  const data = {
+    labels: profitabilityByGenre.map((d) => d.genre),
+    datasets: [
+      {
+        label: "Profitability",
+        data: profitabilityByGenre.map((d) => d.profitability),
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      datalabels: {
+        anchor: "end",
+        align: "top",
+        formatter: Math.round,
+        font: { weigth: "bold", size: 10 },
+      },
+    },
+    scales: { yAxis: { display: false } },
+  };
+
+  return <BarChart data={data} options={options} />;
+};
